dd6a31f3616b3232a8593d4c038d0d4eaa98b0ae
Gwyneth Ashley <gwynethmucio@gmail.com>
2025-11-11 00:56:08 +0800

    installing packages for security

--- models/user.js
+++ models/user.js
@@ -1,18 +1,24 @@
-import mongoose from 'mongoose'
+// models/user.js
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type:String,
-        required: [true, 'First Name is required.']
+        type: String,
+        required: [true, 'First Name is required.'],
+        trim: true
     },
     lastName: {
         type: String,
-        required: [true, 'Last Name is required.']
+        required: [true, 'Last Name is required.'],
+        trim: true
     },
     email: {
         type: String,
         required: [true, 'Email is required.'],
         unique: true,
+        trim: true,
+        lowercase: true,
         validate: {
             validator: (v) => v.endsWith('@sdca.edu.ph'),
             message: 'Email must be an sdca.edu.ph address'
@@ -20,9 +26,33 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: [true, 'Password is required.']
+        required: [true, 'Password is required.'],
+        minlength: 6,
+        select: false
+    },
+    userType: {
+        type: String,
+        enum: ['student', 'faculty', 'canteen_staff'],
+        required: [true, 'User type is required.']
+    },
+    isActive: {
+        type: Boolean,
+        default: true
     }
-})
+}, {
+    timestamps: true
+});
 
-export const UserModel = mongoose.model('User', userSchema)
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
 
+// Method to check password
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+export const UserModel = mongoose.model('User', userSchema);
